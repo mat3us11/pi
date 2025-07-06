a2af9b43f0a3475ff4ec3dfd922aa21aecbda824
OGCmateus <mateus.azevedop5@gmail.com>
2025-07-06 00:03:44 -0300

    header desktop e modal mobile

---
+++ includes/header.php
@@ -0,0 +1,37 @@
+<header>
+    <div class="container">
+        <div class="left">
+            <div class="top-left">
+                <p class="logo">CAMP<span>VIA</span></p>
+            </div>
+            <ul class="nav-links">
+                <li><a href="#"><i class="ph ph-bicycle"></i> Passeios</a></li>
+                <li><a href="#"><i class="ph ph-map-trifold"></i> Roteiros de Viagem</a></li>
+            </ul>
+        </div>
+        <div class="right">
+            <div class="top-right">
+                <button class="btn btn-outline">Cadastre-se</button>
+                <button class="btn btn-fill">Login</button>
+            </div>
+            <div class="icons">
+                <i class="ph ph-clock-counter-clockwise"></i>
+                <i class="ph ph-question"></i>
+            </div>
+        </div>
+    </div>
+</header>
+
+
+<!-- Modal mobile-->
+<button class="hamburger" id="hamburgerBtn" aria-label="Abrir menu">
+    <i class="ph ph-list" id="hamburgerIcon"></i>
+</button>
+<div class="mobile-modal" id="mobileModal" aria-hidden="true">
+    <nav class="modal-content">
+        <ul class="modal-nav">
+            <!-- FIQUEI COM PREGUICA MAS AQUI É ONDE BOTA OS LINKS -->
+    </nav>
+</div>
+
+<script src="https://unpkg.com/@phosphor-icons/web"></script>
