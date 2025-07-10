bfbcf5b047b4e7fd11ba8771168bca9d7f3abc8d
akaz2407 <126501160+akaz1792@users.noreply.github.com>
2025-07-10 01:48:21 +0000

    Footer

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="./assets/css/header.css">
+    <link rel="stylesheet" href="./assets/css/footer.css">
     <link rel="stylesheet" href="./assets/css/index.css">
     <script defer src="./assets/js/modal.js"></script>
     <title>Home</title>
@@ -22,4 +23,5 @@
     <script src="https://unpkg.com/@phosphor-icons/web"></script>
 </body>
 
+        <?php include 'includes/footer.php'; ?>
 </html>
